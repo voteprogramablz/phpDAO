f75a46d0a36fbb578bf1184624ba7e40bd480f3e
André R. de Melo <andrermelo1@hotmail.com>
2022-01-23 23:53:12 -0300

    DAO getlist

--- class/usuario.php
+++ class/usuario.php
@@ -60,6 +60,43 @@ class Usuario
     }
   }
 
+  public static function getList()
+  {
+    $sql = new Sql();
+
+    return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+  }
+
+  public static function search($login)
+  {
+    $sql = new Sql;
+
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+      ':SEARCH' => "%$login%"
+    ));
+  }
+
+  public function login($login, $password)
+  {
+    $sql = new Sql();
+
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+      ":LOGIN" => $login,
+      ":PASSWORD" => $password
+    ));
+
+    if (isset($results[0])) {
+      $row = $results[0];
+
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+    } else {
+      throw new Exception("Login and password invalids.");
+    }
+  }
+
   public function __toString()
   {
     $existAnUser = isset($this->idusuario, $this->deslogin, $this->dessenha, $this->dtcadastro);
--- index.php
+++ index.php
@@ -4,6 +4,23 @@ require_once('config.php');
 
 $usuario = new Usuario;
 
-$usuario->loadById(1);
+// $usuario->loadById(27);
+
+// echo $usuario;
+
+// $lista = Usuario::getList();
+
+// echo (json_encode($lista));
+
+//Carrega uma lista de usuarios buscando pelo login;
+
+// $search = Usuario::search("nd");
+
+// echo json_encode($search);
+
+
+//Carrega um usuario com login e senha
+
+$usuario->login("andrezin", "10215");
 
 echo $usuario;
